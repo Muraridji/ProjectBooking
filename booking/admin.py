63e5dbe17014c0780c774b7d87c6474dd73e3c0c
Muraridji <gusevvlad-11@ukr.net>
2025-02-03 20:41:00 +0100

    place_page added. Prepared urls and views for home_page and place_detail.

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from booking.models import Place, Booking
 
 # Register your models here.
+@admin.register(Place)
+class AdminPlace(admin.ModelAdmin):
+    list_display = ('title', 'price', 'capacity', 'is_available')
+
+@admin.register(Booking)
+class AdminBooking(admin.ModelAdmin):
+    pass
--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from booking.models import Place, Booking
 
 # Create your views here.
+def home_view(request):
+    return render(request, "booking/home_page.html")
+
+def place_page_view(request):
+    places = Place.objects.all()
+    return render(request, 'booking/place_page.html', {'places': places})
+
+def place_detail_view(request):
+    return render(request, "booking/place_detail.html")
