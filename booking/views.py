f4aa0eafb93a5e3d2c57b2d986dd0c53fa10d0f1
Muraridji <gusevvlad-11@ukr.net>
2025-02-12 18:08:58 +0100

    place_page added

--- booking/views.py
+++ booking/views.py
@@ -3,11 +3,8 @@ from booking.models import Place, Booking
 
 # Create your views here.
 def home_view(request):
-    return render(request, "booking/home_page.html")
+    return render(request, "booking/index.html")
 
 def place_page_view(request):
     places = Place.objects.all()
     return render(request, 'booking/place_page.html', {'places': places})
-
-def place_detail_view(request):
-    return render(request, "booking/place_detail.html")
--- config/urls.py
+++ config/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from booking import views
+
+app_name = "booking"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.home_view, name="home"),
-    path('booking/place_page/', views.place_page_view, name="places"),
+    path('', views.home_view, name="home"),
+    path('place_page/', views.place_page_view, name="places"),
+    path("place_page/<int:pk>/", views.place_detail_view, name="place_detail"),
 ]
